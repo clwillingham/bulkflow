b50f527d51e0a12d685243428653014cb4f8f6fd
Chris Willingham <clwillingham@gmail.com>
2024-11-11 23:35:09 -0600

    chore: bump version to 0.1.1

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="bulkflow",
-    version="0.1.0",
+    version="0.1.1",
     author="Chris",
     author_email="clwillingham@gmail.com",
     description="A high-performance CSV to PostgreSQL data loader with chunked processing and error handling",
