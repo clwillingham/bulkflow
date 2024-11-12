35e0334dfe2cac2d28fd28fdc3d37f546d2766ba
Chris Willingham <clwillingham@gmail.com>
2024-11-11 23:31:35 -0600

    fixing missing init file

--- setup.py
+++ setup.py
@@ -21,6 +21,7 @@ setup(
         "Topic :: Database",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
@@ -29,10 +30,10 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8",
+    python_requires=">=3.7",
     install_requires=[
         "psycopg2-binary>=2.9.0",
-        "pandas>=2.0.0"
+        "pandas>=1.0.0,<2.0.0"
     ],
     entry_points={
         'console_scripts': [
